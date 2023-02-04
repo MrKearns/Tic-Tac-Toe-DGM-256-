da84f3ce42a9b76601f60d89e6cc72ace8e25921
MrKearns <jkearns631@students.mchenry.edu>
2023-02-04 16:44:40 -0600

    first

--- Tic-Tac-Toe (DGM-256)/ViewController.swift
+++ Tic-Tac-Toe (DGM-256)/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+//    MARK: --------------- VARIABLES --------------
+    
+    
+    
+//    TODO: --------------- BUTTONS ---------------
+    
+//    top row
+    
+//    middle row
+    
+//    bottom row
+    
+    
+//    clear button
+    
+    
+    
+    
+// --------------- VIEW DID LOAD ---------------
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+//    TODO: --------------- WIN CONDITIONS ---------------
 
 }
 
